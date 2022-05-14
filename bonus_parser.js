17189bdaf94de5e0815772badc0cf27cd2868707
Abdulaziz <azeek.school21@gmail.com>
2022-05-14 09:33:05 +0300

    added some checks

--- bonus_parser.js
+++ bonus_parser.js
@@ -1,17 +1,22 @@
 let axios = require('axios');
-let res = axios.get('https://54.uz/works.json').then(parser);
+let bonus = axios.get('https://54.uz/works.json').then(parser);
 
 function parser(res){
 	let i = 0;
 	let bonus = 0;
+	let data = res.data
 
-	data = res.data
 	while (data.works[i]){
 		bonus = data.works[i].payment.bonus
-		if (bonus > 1000){	
-			console.log(data.works[i].payment.bonus)
-			break
+		if (bonus > 1000){
+			console.log(bonus);
+			break;
 		}
 		i++;
 	}
+
+	if (!bonus){
+		console.log("No bonus with value higher than 1000");
+	};
 };
+//  2 hours earlier I had no idea what axios was. Learnig is my lifelong cowalker.
